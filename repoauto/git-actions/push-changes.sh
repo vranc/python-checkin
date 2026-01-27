c1632133c013b82b0de931c565ef560e43d02f46
Branislav Vranko <branislav.vranko@t-systems.com>
2026-01-27 10:16:05 +0300

    added via script

--- repoauto/git-actions/push-changes.sh
+++ repoauto/git-actions/push-changes.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-test -e
-
 git add .
 git commit -m "added via script"
 git push
--- repoauto/git-actions/test-git-actions.sh
+++ repoauto/git-actions/test-git-actions.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-test -e
-
 ./make-changes.sh
 ./push-changes.sh
 ./fetch-changes.sh
